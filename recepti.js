fdb36f131a286cf2b6ace545d12fa066d5e02c70
Milos <test@mail.com>
2023-07-29 13:27:47 +0200

    made buttons work via JS

--- recepti.js
+++ recepti.js
@@ -1,12 +1,61 @@
 const frwrdBtnEl = document.querySelector('.js-frwrd-btn');
 const bckwrdBtnEl = document.querySelector('.js-bckwrd-btn');
-const imgEl =  document.querySelector('.js-recipe-image');
+const recipeImgContainerEl = document.querySelector('.js-recipe-image-container');
+const startBtnEl = document.querySelector('.js-start-btn');
+
+let book = [
+  {
+    name: 'page1',
+    img: 'image0.jpeg'
+  },
+  {
+    name: 'page2',
+    img: 'image1.jpeg'
+  },
+  {
+    name: 'page3',
+    img: 'image2.jpeg'
+  },
+  {
+    name: 'page4',
+    img: 'image3.jpeg'
+  }
+]
+
+let mainImg = document.createElement('img');
+mainImg.setAttribute('src', book[0].img);
+mainImg.classList.add('recipe-image');
+recipeImgContainerEl.appendChild(mainImg);
+
+let count = 0;
 
 frwrdBtnEl.addEventListener('click', () => {
-  imgEl.src = "image1.jpeg"
-  bckwrdBtnEl.classList.add('enable-bckwrd-btn');
+
+  if ((count+1) < book.length) {
+    nextPage(count);
+    count++;
+  }
+
+  function nextPage(count) {
+    mainImg.src = book[count+1].img
+  }
 });
 
 bckwrdBtnEl.addEventListener('click', () => {
-  imgEl.src = "image0.jpeg"
+
+  if (count < book.length) {
+    prevPage(count);
+    count--;
+  }
+
+  function prevPage(count) {
+    mainImg.src = book[count-1].img
+  }
 });
+
+/*
+startBtnEl.addEventListener('click', () => {
+  mainImg.src = book[0].img
+  count = 0;
+})
+*/
